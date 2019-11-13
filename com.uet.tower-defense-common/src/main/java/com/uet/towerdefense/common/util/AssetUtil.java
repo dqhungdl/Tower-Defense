f1f696b6f8fc885d5b4348009722d5ccd59f739b
dqhungdl <dqhungdl@gmail.com>
2019-11-13 10:28:50 +0700

    add: resources to common

--- com.uet.tower-defense-common/src/main/java/com/uet/towerdefense/common/util/AssetUtil.java
+++ com.uet.tower-defense-common/src/main/java/com/uet/towerdefense/common/util/AssetUtil.java
@@ -3,21 +3,27 @@ package com.uet.towerdefense.common.util;
 import com.uet.towerdefense.common.enums.graphics.GamePlays;
 import javafx.scene.image.Image;
 
+import java.net.URL;
+
 public class AssetUtil {
 
     public static Image getMapImage(int mapId) {
-        return new Image("file:src/main/resources/AssetsKit_2/Maps/map" + mapId + ".png", GamePlays.WIDTH * GamePlays.SPRITE_SIZE, GamePlays.HEIGHT * GamePlays.SPRITE_SIZE, false, false);
+        String fileName = AssetUtil.class.getClassLoader().getResource("AssetsKit_2/Maps/map" + mapId + ".png").toString();
+        return new Image(fileName, GamePlays.WIDTH * GamePlays.SPRITE_SIZE, GamePlays.HEIGHT * GamePlays.SPRITE_SIZE, false, false);
     }
 
     public static Image getTowerImage(String imageId) {
-        return new Image("file:src/main/resources/AssetsKit_2/PNG/Retina/towerDefense_tile" + imageId + ".png", GamePlays.TOWER_SIZE, GamePlays.TOWER_SIZE, false, false);
+        String fileName = AssetUtil.class.getClassLoader().getResource("AssetsKit_2/PNG/Retina/towerDefense_tile" + imageId + ".png").toString();
+        return new Image(fileName, GamePlays.TOWER_SIZE, GamePlays.TOWER_SIZE, false, false);
     }
 
     public static Image getEnemyImage(String imageId) {
-        return new Image("file:src/main/resources/AssetsKit_2/PNG/Retina/towerDefense_tile" + imageId + ".png", GamePlays.ENEMY_SIZE, GamePlays.ENEMY_SIZE, false, false);
+        String fileName = AssetUtil.class.getClassLoader().getResource("AssetsKit_2/PNG/Retina/towerDefense_tile" + imageId + ".png").toString();
+        return new Image(fileName, GamePlays.ENEMY_SIZE, GamePlays.ENEMY_SIZE, false, false);
     }
 
     public static Image getBulletImage(String imageId) {
-        return new Image("file:src/main/resources/AssetsKit_2/PNG/Retina/towerDefense_tile" + imageId + ".png", GamePlays.BULLET_SIZE, GamePlays.BULLET_SIZE, false, false);
+        String fileName = AssetUtil.class.getClassLoader().getResource("AssetsKit_2/PNG/Retina/towerDefense_tile" + imageId + ".png").toString();
+        return new Image(fileName, GamePlays.BULLET_SIZE, GamePlays.BULLET_SIZE, false, false);
     }
 }
